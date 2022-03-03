4c9e388da3cb6f473c5336e85ce13b73e59c8d9c
Kenton Murray <kenton.murray@gmail.com>
2022-03-02 22:35:11 -0500

    can now play against two dumb computers

--- src/PlayGame/play_random_puzzle.py
+++ src/PlayGame/play_random_puzzle.py
@@ -2,6 +2,35 @@ import random
 import re
 import time
 
+def computer_turn(showing, winnings, previous_guesses, turn):
+  # Guess in the order of the alphabet
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  dollar = 0
+  for character in alphabet:
+    if character in previous_guesses:
+      continue
+    if is_vowel(character):
+      if winnings[(turn % 3)] < 250:
+        continue
+      else:
+        winnings[(turn % 3)] = winnings[(turn % 3)] - 250
+        break
+    # Want to choose a consonant ... so spins wheel
+    dollar = spin_wheel()
+    if dollar == 0:
+      print("Computer lost a turn")
+      character = "_"
+      break
+    elif dollar == -1:
+      print("Computer went backrupt")
+      winnings[(turn % 3)] = 0
+      character = "_"
+      break
+    else:
+      print("Computer guessed:", character)
+      break
+  return character, dollar
+
 def get_random_puzzle():
   random_int = random.randint(0,900) # Roughly size of num puzzles in valid
   number = 0
@@ -13,6 +42,20 @@ def get_random_puzzle():
         #print(line)
         return(puzzle, clue, date, game_type)
       number = number + 1
+
+def is_consonant(guess):
+  consonants = "BCDFGHJKLMNPQRSTVWXYZ"
+  if guess in consonants:
+    return True
+  else:
+    return False
+
+def is_vowel(guess):
+  vowels = "AEIOU"
+  if guess in vowels:
+    return True
+  else:
+    return False
 
 def print_board(showing):
   words = showing.split(" ")
@@ -37,21 +80,8 @@ print("The clue is:", clue)
 
 # Mask out word
 showing = puzzle
-#print(showing)
 showing = re.sub(r"[A-Z]","_",showing)
-#print(showing)
 print_board(showing)
-
-#showing = ""
-#for character in puzzle:
-#  if character != " ":
-#    showing = showing + "_ "
-#  else:
-#    showing = showing + "\n"
-#print(showing)
-
-consonants = "BCDFGHJKLMNPQRSTVWXYZ"
-vowels = "AEIOU"
 
 # Play the game
 guess = ""
@@ -61,65 +91,84 @@ turn = 0
 winnings = [0,0,0]
 
 while showing != puzzle:
-
+  # Ends wierd if last letter is guessed and not solved.# TODO
   print("It is player", turn % 3, "'s turn")
 
+  ## Computer playing
+  #if turn % 3 == 1:
+  #  #showing, winnings, previous_guesses = computer_turn(showing, winnings, previous_guesses, turn)
+  #  #turn = turn + 1
+  #  #print("Winnings:", winnings)
+  #  #print_board(showing)
+  #  #continue
+
   # Player decisions
-  decision = input("1: Spin, 2: Buy Vowel, 3: Solve ....  ")
-  if decision == "3":
-    solve = input("Your guess to solve: ...... ").upper() # TODO: clean
-    if solve == puzzle:
-      print("YOU WIN!")
-      print("Player", turn % 3, "won!")
-      exit()
+  if turn % 3 == 0:
+    decision = input("1: Spin, 2: Buy Vowel, 3: Solve ....  ")
+    if decision == "3":
+      solve = input("Your guess to solve: ...... ").upper() # TODO: clean
+      if solve == puzzle:
+        print("YOU WIN!")
+        print("Player", turn % 3, "won!")
+        print("Winnings:", winnings)
+        exit()
+      else:
+        print("Wrong ... next player")
+        turn = turn + 1
+        print_board(showing)
+        continue
+    elif decision == "2":
+      if winnings[(turn % 3)] < 250: # Minimum cost of a vowel
+        print("Sorry .... you don't have enough money. Select 1 or 3")
+        continue
+      winnings[(turn % 3)] = winnings[(turn % 3)] - 250
+      is_one_vowel = False
+      while is_one_vowel != True:
+        vowel = input("Guess a vowel: ").upper()
+        if len(vowel) != 1:
+          print("Guess only one letter")
+        else:
+          is_one_vowel = is_vowel(vowel)
+
+        if not is_one_vowel:
+          print("Not a vowel")
+      guess = vowel
+      dollar = 0
+    elif decision == "1":
+      print("Wheel is spinning ....")
+      print("It landed on ....")
+      time.sleep(2) # Drama!
+      # Spin wheel
+      dollar = spin_wheel()
+      print("....", dollar, "dollars")
+      if dollar == 0:
+        print("Sorry! Lose a turn. Next player")
+        turn = turn + 1
+        continue
+      elif dollar == -1:
+        print("Oh No! Bankrupt!")
+        winnings[(turn % 3)] = 0
+        turn = turn + 1
+        continue
+      is_one_consonant = False
+      while is_one_consonant != True:
+        guess = input("Name a consonant .... ").upper()
+        if len(guess) != 1: 
+          print("Guess only one letter")
+        else:
+          is_one_consonant = is_consonant(guess)
+
+        if not is_one_consonant:
+          print("Not a consonant")
     else:
-      print("Wrong ... next player")
-      turn = turn + 1
-      print_board(showing)
-      continue
-  elif decision == "2":
-    if winnings[(turn % 3)] < 250: # Minimum cost of a vowel
-      print("Sorry .... you don't have enough money. Select 1 or 3")
-      continue
-    winnings[(turn % 3)] = winnings[(turn % 3)] - 250
-    is_one_vowel = False
-    while is_one_vowel != True:
-      vowel = input("Guess a vowel: ").upper()
-      if len(vowel) != 1:
-        print("Guess only one letter")
-      elif vowel not in vowels:
-        print("Not a vowel")
-      else:
-        is_one_vowel = True
-    # Subtract winnings
-    guess = vowel
-  elif decision == "1":
-    print("Wheel is spinning ....")
-    print("It landed on ....")
-    time.sleep(2) # Drama!
-    # Spin wheel
-    dollar = spin_wheel()
-    print("....", dollar, "dollars")
-    if dollar == 0:
-      print("Sorry! Lose a turn. Next player")
-      turn = turn + 1
-      continue
-    elif dollar == -1:
-      print("Oh No! Bankrupt!")
-      winnings[(turn % 3)] = 0
-      turn = turn + 1
-      continue
-    is_one_consonant = False
-    while is_one_consonant != True:
-      guess = input("Name a consonant .... ").upper()
-      if len(guess) != 1: 
-        print("Guess only one letter")
-      elif guess not in consonants:
-        print("Not a consonant")
-      else:
-        is_one_consonant = True
-  else:
-    print("Please choose 1, 2, or 3")
+      print("Please choose 1, 2, or 3")
+
+  # Computer playing
+  elif turn % 3 == 1:
+    guess, dollar = computer_turn(showing, winnings, previous_guesses, turn)
+  elif turn % 3 == 2:
+    guess, dollar = computer_turn(showing, winnings, previous_guesses, turn)
+
 
   # Update board
   previous_guesses.append(guess)
@@ -128,7 +177,9 @@ while showing != puzzle:
     if(char == guess):
         correct_places.append(pos)
   #print(correct_places)
-  if len(correct_places) < 1:
+  if guess == "_": # Hacky way to say the comp got it wrong
+    turn = turn + 1
+  elif len(correct_places) < 1:
     print("Sorry, not in the puzzle ... next player")
     turn = turn + 1
   winnings[(turn % 3)] = winnings[(turn % 3)] + (dollar * len(correct_places))
